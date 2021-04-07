92020d9772c042913e1baa38d79164c7ec4e2c96
Patrick Lee <patricklee1138@live.com>
2021-04-07 15:59:50 -0700

    add turn switching from player 1 to player 2
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,29 +14,51 @@ function Board() {
   ];
 }
 
-// 
-Board.prototype.addMark = function(mark, boardSpace) {
-  // const targetSpace = this.spaces[boardSpace];
-  if (this.spaces[boardSpace] === "") { // If the player's chosen space is empty...
-    this.spaces[boardSpace] = mark; // ...place their mark
+function Player(playerNum, playerMark) {
+  this.playerNum = playerNum;
+  this.playerMark = playerMark;
+};
+// Instantiate a new board to begin a game
+const currentGame = new Board();
+// if (player1Turn === true) {
+//   playerMark = "X"
+// } else if (player1Turn === false) {
+//   playerMark = "O"
+// }
+let player1 = new Player(1, "X");
+let player2 = new Player(2, "O");
+
+Player.prototype.addMark = function(boardSpace) {
+  if (currentGame.spaces[boardSpace] === "") { // If the player's chosen space is empty...
+    currentGame.spaces[boardSpace] = this.playerMark; // ...place their mark
+    console.log("Player " + this.playerMark + " made a mark")
   } else { // Otherwise, tell them the space is taken
-    console.log("Sorry, Player " + mark + "! This space is taken!")
+    console.log("Sorry! This space is taken!")
   }
-  console.log("Current board state: " + this.spaces) // Print the current board (an ugly array for now)
+  switchTurn(this.playerNum);
+  // check if mark wins the game
+  // check if board is full/cat's game
+  // if neither, change mark to next player?
 };
 
-
-
-
-// function Player() {
-
-// };
-
+function switchTurn(playerNum) {
+  if (playerNum === 1) {
+    playerMark = "O";
+  } else if (playerNum === 2) {
+    playerMark = "X";
+  } else {
+    console.log("Error: no player selected");
+  };
+};
 
 // Current console testing commands
-// Instantiate a new board to begin a game
-const thisGame = new Board();
-let playerMark = "X";
-let targetSpace = 4;
-console.log("Starting board state: " + thisGame.spaces)
-thisGame.addMark(playerMark, targetSpace);
+
+// Show the empty board before any commands:
+console.log("Starting board state: " + currentGame.spaces)
+
+// Add a player mark:
+player1.addMark(4);
+player2.addMark(7);
+
+// Show the current board state:
+console.log("Current board state: " + currentGame.spaces)
