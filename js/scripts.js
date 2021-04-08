84eb0483aff90d538798941fbaf56b0f8b6014e3
Maxwell Meyer <maxreadswell@gmail.com>
2021-04-08 15:54:06 -0700

    add button to reset game, clean up comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-// Tic-Tac-Toe
-// Game board object
 function Board() {
   this.spaces = [
     " ",
@@ -20,21 +18,16 @@ function Player(playerNum, playerMark) {
   this.playerMark = playerMark;
 };
 
-// Instantiate a new board to begin a game
-const currentGame = new Board();
-
-// Instantiate players 1 & 2
-const player1 = new Player(1, "X");
-const player2 = new Player(2, "O");
-
-// Designate the first active player
-let currentPlayer = player1;
+    const currentGame = new Board();
+    const player1 = new Player(1, "X");
+    const player2 = new Player(2, "O");
+    let currentPlayer = player1;
 
 Player.prototype.addMark = function(space) {
   if (currentGame.active === true) {
     const board = currentGame.spaces;
     const player = this.playerNum;
-    if (board[space] === " ") { // If the player's chosen space is blank...
+    if (board[space] === " ") { 
       board[space] = this.playerMark; // ...place their mark
       console.log("Player " + player + " made a mark");
       console.log(renderBoardConsole(currentGame.spaces));
@@ -42,7 +35,6 @@ Player.prototype.addMark = function(space) {
       if (result === "win") {
         console.log("Player " + player + " wins!");
         return `Player ${player} wins!`;
-        // return `Player ${player} wins!`;
       } else if (result === "draw") {
         console.log("Cat's game!")
         return "CATS!!!";
@@ -68,24 +60,17 @@ function resultCheck(player) {
   console.log("Checking results...")
   const boardState = currentGame.spaces;
   const winningSpaces = [
-    // Horizontal win streaks:
-    [0, 1, 2],// To look at a *single* value, you'd use:
-    [3, 4, 5],// winningSpaces[1][0] to get 3...
-    [6, 7, 8],// winningSpaces[2][1] to get 7... etc.
-    // Vertical win streaks:
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
-    // Diagonal win streaks:
     [0, 4, 8],
     [2, 4, 6]
-  ];
-  // Check for a win:
+  ]; // Check for a win:
   for (let i = 0; i < winningSpaces.length; i++) {
     let streak = winningSpaces[i];
-    // Below, "streak[0]" would process as "winningSpaces[i][0]",
-    // so it's looking at the *first* number in an array of three numbers,
-    // then comparing it to the next two to see if they all match -- for a win.
     if (
       boardState[streak[0]] !== " "
       && boardState[streak[0]] === boardState[streak[1]]
@@ -96,7 +81,6 @@ function resultCheck(player) {
       return "win";
     };
   };
-  // Check for a draw:
   let catsGame = true; // Assume it's a draw -- until we find a blank space
   for (let j = 0; j < boardState.length; j++) {
     if (boardState[j] === " ") { // If a blank space *is* found...
@@ -104,14 +88,11 @@ function resultCheck(player) {
       break; // ...and we can end the loop early.
     }
   }
-  if (catsGame === true) { // If catsGame wasn't set to false in the loop,
+  if (catsGame === true) {
     currentGame.active = false;
-    return "draw"; // then we know it's a draw.
+    return "draw"; 
   }
 };
-
-
-
 
 // User Interface Logic
 ///////////////////////
@@ -130,28 +111,23 @@ function renderBoardConsole(spaces) {
   return boardString;
 };
 
-
 $(document).ready(function() {
-  // do the stuff
-  // get the id (e.g. "b0")
-  // remove the "b"
-  // update the screen based on the board array at e.g. "0"
-  // let theid = $(someElement).attr('id'); Solution from https://forum.jquery.com/topic/get-id-from-element-in-string
-
-  // function attachContactListeners() {
-  //   $("ul#contacts").on("click", "li", function() {
-  //     showContact(this.id);     // <--- This is new!
-  //   });
-  // };
   $("#gameBoard").on("click", ".box", function() {
     let originalId = this.id; // id === e.g. "b0"
     let newId = parseInt(originalId.replace('b', ''));
-    let resultDiv = $(".result");
+    let resultDiv = $("#result");
+    let resultMessage = $(".result");
     let currentMark = currentPlayer.addMark(newId);
     if (currentMark === "CATS!!!" || currentMark === `Player ${currentPlayer.playerNum} wins!`) {
-      resultDiv.slideDown(1000);
+      resultMessage.slideDown(1000);
       resultDiv.html(currentMark);
     }
     $("#" + originalId).html(currentGame.spaces[newId]);
   });
+  $("#reset").click(function(event) {
+    event.preventDefault();
+    $(".result").slideUp();
+    clearBoard();
+  });
 });
+
